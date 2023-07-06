8909ce05981a974e7eb60ed93333c0c8053f9f2d
Sagar Timalsena <stimalsena155@gmail.com>
2023-07-06 17:26:06 +0545

    pluralsight course learn

---
+++ OneDrive/Desktop/pluralsight/InitialApplication/InitialApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InitialApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
