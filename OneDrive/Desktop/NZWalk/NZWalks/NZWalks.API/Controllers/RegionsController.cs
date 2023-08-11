4345511caaefdac593cfdbc24ea84fc8177b5ad4
Sagar Timalsena <stimalsena155@gmail.com>
2023-08-11 17:36:59 +0545

    added auto mapper functionality

--- OneDrive/Desktop/NZWalk/NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ OneDrive/Desktop/NZWalk/NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
@@ -15,26 +15,26 @@ namespace NZWalks.API.Controllers
     {
         private readonly NZWalksDbContext dbContext;
         private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
 
-        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository)
+        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository, IMapper mapper)
         {
             this.dbContext = dbContext;
             this.regionRepository = regionRepository;
+            this.mapper = mapper;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var regionsDomain = await dbContext.Regions.ToListAsync();
+            //get data from database - domain models
+            var regionsDomain = await regionRepository.GetAllAsync();
 
-            var regionsDto = regionsDomain.Select(regionDomain => new RegionDto
-            {
-                Id = regionDomain.Id,
-                Code = regionDomain.Code,
-                Name = regionDomain.Name,
-                RegionImageUrl = regionDomain.RegionImageUrl
-            }).ToList();
 
+            //map domain model to DTOS
+            var regionsDto = mapper.Map<List<RegionDto>>(regionsDomain);
+
+            //Return DTOs
             return Ok(regionsDto);
         }
 
@@ -50,18 +50,10 @@ namespace NZWalks.API.Controllers
             {
                 return NotFound();
             }
-
-            var regionDto = new RegionDto
-            {
-                Id = regionDomain.Id,
-                Code = regionDomain.Code,
-                Name = regionDomain.Name,
-                RegionImageUrl = regionDomain.RegionImageUrl
-            };
-
-            return Ok(regionDto);
+            //Return DTO back to client
+            return Ok(mapper.Map<RegionDto>(regionDomain));
         }
-
+        //create region 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
         {
